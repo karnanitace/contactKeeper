ed9af12952cfdc4cb802503f4c0f2787a32cddc6
Nitesh Karna <karnanitace16@gmail.com>
2020-08-25 17:41:15 +0545

    removed initial route

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.send({ msg: "Welcome to Contact Keper API..." })
-);
-
 //Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
